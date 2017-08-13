634eaf3f0f4283896aa8dd698cfe0f193bac7662
Konstantin Ignatiev <QfpbC7u3V13qJUop@i2pmail.org>
2017-08-13 22:07:40 +0300

    [WIP] Sublicensing.

--- pages/handlers/vc-auth.php
+++ pages/handlers/vc-auth.php
@@ -1,13 +1,29 @@
 <?php
-require_once("../../../../wp-load.php");
-//for logged in users
-add_action('wp_ajax_my_action', 'my_action_callback');
-//for not logged in users
-add_action('wp_ajax_nopriv_my_action', 'my_action_callback');
+  /*
+   * Encorrientador Unido - are collection of web-based software plugins for
+   * popular open-source content management systems. It has a purpose of providing
+   * secure browser-based video chat with database management features and
+   * account registration independent from third-party content delivery networks.
+   * This software is based on PubNub WebRTC video chat source code.
+   * Copyright (C) 2017  Andrei Shishkin <QfpbC7u3V13qJUop@i2pmail.org>
+   * 
+   * This program is free software: you can redistribute it and/or modify
+   * it under the terms of the GNU General Public License as published by
+   * the Free Software Foundation, either version 3 of the License, or
+   * (at your option) any later version.
+   * 
+   * This program is distributed in the hope that it will be useful,
+   * but WITHOUT ANY WARRANTY; without even the implied warranty of
+   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   * GNU General Public License for more details.
+   * 
+   * You should have received a copy of the GNU General Public License
+   * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   */
+?>
 
-function my_action_callback() {
-  //your function here
-  echo $_GET["request_name"];
-  exit; //always call exit at the end of a WordPress ajax function
-}
+
+
+<?php
+  require_once("../../../../wp-load.php");
 ?>
